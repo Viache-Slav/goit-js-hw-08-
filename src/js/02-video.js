5e65fe1e6b5ee92e686198f832c51deb57c26df0
Viache-Slav <slavikfedko@gmail.com>
2023-08-27 20:40:44 +0200

    dark-mode

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,6 @@
 import Player from '@vimeo/player';
 import {throttle} from 'lodash';
+import { toggleDarkMode } from "./dark-mode.js";
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
